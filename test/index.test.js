9ace6590e92a2f9c750db9c657da95178185971b
Sean Nelson <smnelsonn@gmail.com>
2019-04-02 16:00:34 -0700

    initial test and setup

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const { MemoryDatabase } = require('../lib/index.js');
+
+describe('create memory class', () => {
+  it('create empty storage', () => {
+    const memory = new MemoryDatabase();
+    expect(memory.store).toEqual({});
+  });
+});
