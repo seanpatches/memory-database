53b405ea42a4e95d34abb6d72c9da188c555c0a7
Sean Nelson <smnelsonn@gmail.com>
2019-04-03 09:27:49 -0700

    finished find, display all in storage

--- test/index.test.js
+++ test/index.test.js
@@ -20,4 +20,20 @@ describe('create memory class', () => {
     const id = copy._id;
     expect(memory.findById(id)).toEqual({ ...copy });
   });
+
+  it('find by id', () => {
+    const memory = new MemoryDatabase();
+    const obj = { name: 'Superman' };
+    memory.create(obj);
+    expect(memory.findById(222)).toEqual(null);
+  });
+
+  it('find', () => {
+    const memory = new MemoryDatabase();
+    const dog = memory.create({ name: 'bob', age: 5 });
+    const cat = memory.create({ name: 'kitty', age: 24 });
+    expect(memory.find()).toEqual([{ ...dog }, { ...cat }]);
+  });
+
+  
 });
