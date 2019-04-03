8191976846820bc51d7aeda71be82056e2b416ed
Sean Nelson <smnelsonn@gmail.com>
2019-04-02 17:14:50 -0700

    first test

--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,11 @@ describe('create memory class', () => {
     const memory = new MemoryDatabase();
     expect(memory.store).toEqual({});
   });
+
+  it('adds an name obj to the store', () => {
+    const memory = new MemoryDatabase();
+    const obj = { name: 'Superman' };
+    const copy = memory.create(obj);
+    expect(copy.name).toEqual('Superman');
+  });
 });
